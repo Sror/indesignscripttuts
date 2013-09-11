4cc9ee6bf88f9fec82a36165534cf484a4be866a
Andrea Kennedy <asrkennedy@gmail.com>
2013-09-11 09:51:24 +0300

    created first test file for new doc and hello world textbox

---
+++ test.jsx
@@ -0,0 +1,4 @@
+var myDocument = app.documents.add();
+var myTextFrame = myDocument.pages.item(0).textFrames.add();
+myTextFrame.geometricBounds = ["6p", "6p", "24p", "24p"];
+myTextFrame.contents = "Hello World!";
